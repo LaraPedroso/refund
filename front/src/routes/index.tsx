a6b1c181d0c44de63981746aa19da273961c916a
Lara <pedroso5@hotmail.com>
2025-04-03 19:12:26 -0300

    fix: back insert

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -7,13 +7,13 @@ import { Loading } from '../components/Loading'
 const isLoading = false
 const session = {
     user: {
-        role:"employe"
+        role:"employee"
     }
 }
 export function Routes() {
     function Route() {
         switch (session.user.role) {
-            case "employe":
+            case "employee":
                 return <EmployeeRoutes/>
             case "manager":
                 return <ManagerRoutes/>
