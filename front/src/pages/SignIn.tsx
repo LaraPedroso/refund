030f1077d97069a8577aff47245e50b5e94fb982
lara <pedroso5@hotmail.com>
2025-04-08 16:37:55 -0300

    fix: signIn and signUp

--- front/src/pages/SignIn.tsx
+++ front/src/pages/SignIn.tsx
@@ -1,37 +1,53 @@
-import { useState } from "react";
+import { useActionState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useActionData } from "react-router";
 
 export function SignIn() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
-    function onSubmit(e: React.FormEvent){
-        e.preventDefault()
-        console.log(email, password)
+    // useActionData é um hook que retorna o estado atual da ação
+    // formAction dispara a ação quando o formulário é enviado ( action={formAction} ==> signIn)
+    // state é o estado atual da ação
+    const [state, formAction, isLoading] = useActionState(signIn, {
+        email: "",
+        password: "",
+    });
 
+    // formData é um objeto que contém os dados do formulário
+    function signIn(prevState: any, formData: FormData) {
+        // recupera o conteúdo pelo nome sem utilizar estados ( useState )
+        const email = formData.get("email");
+        const password = formData.get("password");
+        console.log(prevState);
+
+        return { email, password };
     }
     return (
-        <form 
-            onSubmit={onSubmit}
-            className="w-full flex flex-col gap-4"
-        >
-            <Input 
-                required 
-                legend="E-mail" 
-                type="email" 
+        <form action={formAction} className="w-full flex flex-col gap-4">
+            <Input
+                required
+                name="email"
+                legend="E-mail"
+                type="email"
                 placeholder="seu@email.com"
-                onChange={(e) => setEmail(e.target.value)}
+                defaultValue={String(state?.email)}
             />
-            <Input 
-                required 
-                legend="Senha" 
-                type="password" 
+            <Input
+                required
+                name="password"
+                legend="Senha"
+                type="password"
                 placeholder="****"
-                onChange={(e) => setPassword(e.target.value)}
-                />
-            <Button type="submit" isLoading={isLoading}>Entrar</Button>
-            <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
+                defaultValue={String(state?.password)}
+            />
+            <Button type="submit" isLoading={isLoading}>
+                Entrar
+            </Button>
+            <a
+                href="/signup"
+                className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+            >
+                Criar conta
+            </a>
         </form>
-    )
+    );
 }
--- front/src/pages/SignUp.tsx
+++ front/src/pages/SignUp.tsx
@@ -2,83 +2,111 @@ import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { z, ZodError } from "zod";
+import { api } from "../services/api";
+import { useNavigate } from "react-router";
+import { AxiosError } from "axios";
 
-
-const singUpSchema = z.object({
-    name: z.string().trim().min(1, {message: "Informe o nome"}),
-    email: z.string({message: "Informe o e-mail"}).email({message:"E-mail inválido"}),
-    password: z.string().min(6, {message: "Senha deve ter pelo menos 6 digitos"}),
-    passwordConfirm: z.string({message: "Confirme a senha"}),
-}).refine((data) => data.password === data.passwordConfirm, {
-    message: "As senhas não corresponde",
-    path: ['passwordConfirm'],
-})
-
+const singUpSchema = z
+    .object({
+        name: z.string().trim().min(1, { message: "Informe o nome" }),
+        email: z
+            .string({ message: "Informe o e-mail" })
+            .email({ message: "E-mail inválido" }),
+        password: z
+            .string()
+            .min(6, { message: "Senha deve ter pelo menos 6 digitos" }),
+        passwordConfirm: z.string({ message: "Confirme a senha" }),
+    })
+    .refine((data) => data.password === data.passwordConfirm, {
+        message: "As senhas não corresponde",
+        path: ["passwordConfirm"],
+    });
 
 export function SignUp() {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirm, setPasswordConfirm] = useState("")
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
 
-    const [isLoading, setIsLoading] = useState(false)
-    function onSubmit(e: React.FormEvent){
+    const [isLoading, setIsLoading] = useState(false);
 
+    const navigate = useNavigate();
+
+    async function onSubmit(e: React.FormEvent) {
+        e.preventDefault();
         try {
-            setIsLoading(true)
+            setIsLoading(true);
             const data = singUpSchema.parse({
                 name,
                 email,
                 password,
-                passwordConfirm
-            })
-            
+                passwordConfirm,
+            });
+
+            await api.post("/users", data);
+
+            if (
+                confirm("Cadastro realizado com sucesso! Deseja fazer login?")
+            ) {
+                navigate("/");
+            }
         } catch (error) {
-            if(error instanceof ZodError){
-                return alert(error.issues[0].message)
+            console.log(error);
+
+            // erro do zod
+            if (error instanceof ZodError) {
+                return alert(error.issues[0].message);
             }
 
-            alert('Não foi possivel cadastrar!')
-        } finally {
-            setIsLoading(false)
-        }
+            // erro da api
+            if (error instanceof AxiosError) {
+                return alert(error.response?.data.message);
+            }
 
+            alert("Não foi possível cadastrar!");
+        } finally {
+            setIsLoading(false);
+        }
     }
     return (
-        <form 
-            onSubmit={onSubmit}
-            className="w-full flex flex-col gap-4"
-        >
-            <Input 
-                required 
-                legend="Nome" 
-                type="text" 
+        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+            <Input
+                required
+                legend="Nome"
+                type="text"
                 placeholder="Digite seu nome"
                 onChange={(e) => setName(e.target.value)}
             />
-            <Input 
-                required 
-                legend="E-mail" 
-                type="email" 
+            <Input
+                required
+                legend="E-mail"
+                type="email"
                 placeholder="seu@email.com"
                 onChange={(e) => setEmail(e.target.value)}
             />
-            <Input 
-                required 
-                legend="Senha" 
-                type="password" 
+            <Input
+                required
+                legend="Senha"
+                type="password"
                 placeholder="****"
                 onChange={(e) => setPassword(e.target.value)}
             />
-            <Input 
-                required 
-                legend="Confime sua senha" 
-                type="password" 
+            <Input
+                required
+                legend="Confime sua senha"
+                type="password"
                 placeholder="****"
                 onChange={(e) => setPasswordConfirm(e.target.value)}
             />
-            <Button type="submit" isLoading={isLoading}>Entrar</Button>
-            <a href="/" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Já tenho uma conta</a>
+            <Button type="submit" isLoading={isLoading}>
+                Entrar
+            </Button>
+            <a
+                href="/"
+                className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+            >
+                Já tenho uma conta
+            </a>
         </form>
-    )
+    );
 }
